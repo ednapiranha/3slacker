1ab13e1d14823509efb8aa212831bbabe8e7b0f6
Jen <jfong@mozilla.com>
2016-05-15 17:01:43 -0700

    new message

--- slacker/messages.js
+++ slacker/messages.js
@@ -42,6 +42,12 @@ function sendResponse(data) {
       weather.getWeather(data, rtm, sockets);
       return;
     }
+
+    if (data.text.match(/what do you think/gi)) {
+      sockets.emit('action', 'happy');
+      sockets.emit('message', "CALL THE COPS I DON'T GIVE A FUCK");
+      return;
+    }
   }
 
   if (data.text.match(/balloon$/gi)) {
